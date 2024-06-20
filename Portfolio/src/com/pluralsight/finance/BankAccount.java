61444fbe2e21b65e1ec0cebaa3cd06a7fcd3005e
Jappdev9 <167275833+Jappdev9@users.noreply.github.com>
2024-06-20 09:34:42 -0400

    Add files via upload

---
+++ Portfolio/src/com/pluralsight/finance/BankAccount.java
@@ -0,0 +1,29 @@
+package com.pluralsight.finance;
+public class BankAccount implements Valuable{
+
+    public static void main(String[] args) {
+
+    }
+    private String name;
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String name, String accountNumber, double balance){
+        this.name=name;
+        this.accountNumber=accountNumber;
+        this.balance=balance;
+    }
+
+    public void deposit(double amount){
+        balance+=amount;
+    }
+
+    public void withdraw(double amount){
+        balance-=amount;
+    }
+
+    @Override
+    public double getValue(){
+        return balance;
+    }
+}
---
+++ Portfolio/src/com/pluralsight/finance/Valueable.java
@@ -0,0 +1,11 @@
+package com.pluralsight.finance;
+
+public class Valueable {
+
+    public static void main(String[] args) {
+
+        public interface Valuable {
+            public double getValue();
+        }
+    }
+}
